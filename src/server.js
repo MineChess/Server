bfa8817026123208d450e3df70768fbbc6e89350
saile336 <elias.nybondas@gmail.com>
2024-10-11 13:37:02 +0300

    oopsie

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-    origin: process.env.DEV_ORIGIN || 'http://localhost:3000'
+    origin: process.env.DEV_ORIGIN
 }))
 
 app.get('/', (req, res) => {
