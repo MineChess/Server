caa915eac4e3a28664133970b9a85c269f825906
saile336 <elias.nybondas@gmail.com>
2024-10-22 14:34:15 +0300

    projekt 2, tagit bort CORS

--- src/server.js
+++ src/server.js
@@ -5,9 +5,9 @@ const PORT = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
 
-app.use(cors({
+/*app.use(cors({
     origin: process.env.DEV_ORIGIN
-}))
+}))*/
 
 app.get('/', (req, res) => {
     res.send("Nothing of note here.")
